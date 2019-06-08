ff7fd03b010bee74aa570d86e212cda53383f996
kawamotomasa <0316kawamoto@gmail.com>
2019-06-08 07:08:00 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def  create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+      #保存
+    else
+      render :new
+    end
+    
+  end
+  
+  private
+  
+  def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
