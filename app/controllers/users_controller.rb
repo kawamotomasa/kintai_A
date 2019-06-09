8a64aa15586d376eefab518919f78e5aee6bec0e
kawamotomasa <0316kawamoto@gmail.com>
2019-06-09 02:14:39 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,18 +11,26 @@ class UsersController < ApplicationController
   def  create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
       #保存
     else
+      flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
-    
   end
+  
+  def destroy
+    log_out
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_path
+  end
+end
   
   private
   
   def user_params
   params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-end
+
