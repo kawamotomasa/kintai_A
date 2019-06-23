f6cfdaea025d5594c9b0b2dc5f0a55c1b8021031
kawamotomasa <0316kawamoto@gmail.com>
2019-06-23 10:41:23 +0000

    Finish add-user-attendances

---
+++ app/models/attendance.rb
@@ -0,0 +1,13 @@
+class Attendance < ApplicationRecord
+  belongs_to :user
+  
+  validates :worked_on, presence: true
+  validates :note, length: { maximum: 50 }
+  
+  validate :finished_at_is_invalid_without_a_started_at
+  
+  def finished_at_is_invalid_without_a_started_at
+      errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+  end
+  
+end
