63e4cb092324fe95f0a0ee486d6679986b0ca692
kawamotomasa <0316kawamoto@gmail.com>
2019-07-16 22:45:26 +0000

    Finish kengen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   #before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   #before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
-  before_action :admin_or_correct_user, only: [:show,:edit,:update]
+  before_action :admin_or_correct_user, only: [:index,:show,:edit,:update]
   before_action :set_one_month, only: :show
 
   def index
